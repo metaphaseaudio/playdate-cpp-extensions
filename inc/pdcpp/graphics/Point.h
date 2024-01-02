15d0b366f62714b70f84c1edab93f135531d7349
Matt Zapp <info@metaphaseaudio.com>
2024-01-01 19:31:42 -0500

    * adding methods to the Point class to to rotational calculation for   polygons

--- inc/pdcpp/graphics/Point.h
+++ inc/pdcpp/graphics/Point.h
@@ -25,6 +25,11 @@ namespace pdcpp
             return ::sqrtf(a2 + b2);
         }
 
+        [[ nodiscard ]] Point<float> rotated(const Point<T>& center, float radians) const;
+
+        [[ nodiscard ]] Point<T> withX(T newX) const;
+        [[ nodiscard ]] Point<T> withY(T newY) const;
+
         [[ nodiscard ]] Point<float> toFloat() const { return {float(x), float(y)}; }
         [[ nodiscard ]] Point<int> toInt() const { return {int(x), int(y)}; }
 
@@ -36,4 +41,27 @@ namespace pdcpp
 
         T x, y;
     };
+
+    template<typename T>
+    Point<T> Point<T>::withX(T newX) const
+    {
+        return Point<float>(newX, y);
+    }
+
+    template<typename T>
+    Point<T> Point<T>::withY(T newY) const
+    {
+        return Point<float>(x, newY);
+    }
+
+    template<typename T>
+    Point<float> Point<T>::rotated(const Point<T>& center, float radians) const
+    {
+        float c = ::cosf(radians);
+        float s = ::sinf(radians);
+        auto p = *this - center;
+
+        const auto h = distance(center);
+        return Point<T>(p.x * c - p.y * s, p.x * s + p.y * c) + center;
+    }
 }
