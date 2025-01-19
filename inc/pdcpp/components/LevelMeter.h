783552a45c993ebb7179f2c96195131fcc6862e8
Matt Zapp <info@metaphaseaudio.com>
2025-01-19 15:00:17 -0500

    Level Meter is looking a lot better now

--- inc/pdcpp/components/LevelMeter.h
+++ inc/pdcpp/components/LevelMeter.h
@@ -12,10 +12,11 @@ namespace pdcpp
         : public Component
     {
     public:
+        explicit LevelMeter(int refreshRate, float lowerBound=0.002f);
         void update(const int16_t* data, int len);
 
     protected:
         void draw() override;
-        float m_CurrentRMS;
+        float m_RefreshRate, m_LowerBound, m_LowerBoundDecibel, m_Sum{0}, m_LenOfSum{0}, m_CurrentRMS{0};
     };
 }
--- src/components/LevelMeter.cpp
+++ src/components/LevelMeter.cpp
@@ -4,16 +4,15 @@
 #include "pdcpp/components/LevelMeter.h"
 #include "pdcpp/graphics/Graphics.h"
 #include "pdcpp/graphics/Colors.h"
+#include "pdcpp/core/GlobalPlaydateAPI.h"
 
-static const float kMinimumRMSdB =  pdcpp::gainToDB(std::numeric_limits<float>::min());
 
 void pdcpp::LevelMeter::draw()
 {
     auto localBounds = getBounds();
     pdcpp::Graphics::fillRectangle(localBounds.toInt(), pdcpp::Colors::white);
-    int height = localBounds.height * (1.0f - (m_CurrentRMS / kMinimumRMSdB));
+    int height = localBounds.height * (1.0f - (m_CurrentRMS / m_LowerBoundDecibel));
 
-//    int height = localBounds.height * m_CurrentRMS;
     if (height > 0)
         { pdcpp::Graphics::fillRectangle(localBounds.removeFromBottom(height).toInt(), pdcpp::Colors::solid50GrayA); }
     pdcpp::Graphics::drawRectangle(getBounds().toInt(), pdcpp::Colors::black);
@@ -21,12 +20,24 @@ void pdcpp::LevelMeter::draw()
 
 void pdcpp::LevelMeter::update(const int16_t* data, int len)
 {
-    float rms = 0;
     for (int i = len; --i >=0;)
     {
         float sample = float(data[i]) / INT16_MAX;
-        rms += sample * sample;
+        m_Sum += sample * sample;
     }
-//    m_CurrentRMS = std::sqrtf(rms / len);
-    m_CurrentRMS = rms > 0 ? pdcpp::gainToDB(std::sqrtf(rms / len)) : kMinimumRMSdB;
+    m_LenOfSum += len;
+    if (m_LenOfSum >= kAudioHardwareSampleRate / m_RefreshRate)
+    {
+        float rms = std::sqrtf(m_Sum / m_LenOfSum);
+        m_CurrentRMS = rms > m_LowerBound ? pdcpp::gainToDB(rms) : m_LowerBoundDecibel;
+        m_Sum = 0;
+        m_LenOfSum = 0;
+    }
 }
+
+pdcpp::LevelMeter::LevelMeter(int refreshRate, float lowerBound)
+    : m_RefreshRate(refreshRate)
+    , m_LowerBound(lowerBound)
+    , m_LowerBoundDecibel(pdcpp::gainToDB(m_LowerBound))
+{}
+
